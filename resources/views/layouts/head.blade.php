1dbf9745311f89b5f88c25d4cb7eea843068c760
Roots <roots@Rootss-MacBook-Pro.local>
2018-09-20 20:16:02 +0630

    update head

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -23,7 +23,7 @@
 
         <div class="wrap_header">
             <!-- Logo -->
-            <a href="index.html" class="logo">
+            <a href="{{ url('/') }}" class="logo">
                 <img src="{{url('frontend/images/icons/logo_bk.png')}}" alt="IMG-LOGO">
             </a>
 
@@ -118,8 +118,8 @@
     <!-- Header Mobile -->
     <div class="wrap_header_mobile">
         <!-- Logo moblie -->
-        <a href="index.html" class="logo-mobile">
-            <img src="{{url('frontend/images/icons/logo.jpeg')}}" alt="IMG-LOGO">
+        <a href="{{ url('/') }}" class="logo-mobile">
+            <img src="{{url('frontend/images/icons/logo_bk.png')}}" alt="IMG-LOGO">
         </a>
 
         <!-- Button show menu -->
@@ -127,14 +127,20 @@
             <!-- Header Icon mobile -->
             <div class="header-icons-mobile">
                 <a href="#" class="header-wrapicon1 dis-block">
-                    <img src="{{url('frontend/images/icons/icon-header-01.png')}}" class="header-icon1" alt="ICON">
+                    
                 </a>
 
                 <span class="linedivide2"></span>
 
                 <div class="header-wrapicon2">
-                    <img src="{{url('frontend/images/icons/icon-header-02.png')}}" class="header-icon1 js-show-header-dropdown" alt="ICON">
-                    <span class="header-icons-noti">0</span>
+                    <a href="{{ url('/cart') }}">
+                        <img src="{{url('frontend/images/icons/icon-header-02.png')}}" class="header-icon1 js-show-header-dropdown" alt="ICON">
+                        @if(Cart::content()) 
+                            <span class="header-icons-noti">{{ Cart::count() }}</span>  
+                        @else
+                            <span class="header-icons-noti">0</span>    
+                        @endif
+                    </a>
 
                     <!-- Header cart noti -->
                     <div class="header-cart header-dropdown">
@@ -225,21 +231,18 @@
             <ul class="main-menu">
                 <li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
                     <span class="topbar-child1">
-                        Free shipping for standard order over $100
+                        Free shipping for standard order over 100
                     </span>
                 </li>
 
                 <li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
                     <div class="topbar-child2-mobile">
                         <span class="topbar-email">
-                            fashe@example.com
+                            Nyo Lay Htike
                         </span>
 
                         <div class="topbar-language rs1-select2">
-                            <select class="selection-1" name="time">
-                                <option>USD</option>
-                                <option>EUR</option>
-                            </select>
+                            
                         </div>
                     </div>
                 </li>
@@ -255,37 +258,62 @@
                 </li>
 
                 <li class="item-menu-mobile">
-                    <a href="index.html">Home</a>
+                    <a href="{{url('/')}}">Home</a>
+                </li>
+
+                <li class="item-menu-mobile">
+                    <a href="{{url('/about')}}">About</a>
+                </li>
+
+                <li class="item-menu-mobile">
+                    <a href="{{url('/contact')}}">Contact</a>
+                </li>
+
+                @guest
+                    <li class="item-menu-mobile">
+                        <a href="{{url('/login')}}">Login</a>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="" class="header-wrapicon1 dis-block">
+                            <img src="{{url('frontend/images/icons/icon-header-01.png')}}" class="header-icon1" alt="ICON">
+                        </a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
+
+
+                <li class="item-menu-mobile">
+                    <a href="#">Delivery Price</a>
                     <ul class="sub-menu">
-                        <li><a href="index.html">Homepage V1</a></li>
-                        <li><a href="home-02.html">Homepage V2</a></li>
-                        <li><a href="home-03.html">Homepage V3</a></li>
+                        <li><a href="#">Sanchaung - 1000</a></li>
+                        <li><a href="#">KyiMyintDine - 1500</a></li>
+                        <li><a href="#">Alone - 1800</a></li>
+                        <li><a href="#">Mayangone - 2000</a></li>
+                        <li><a href="#">Hlaing - 2500</a></li>
+                        <li><a href="#">Insein - 3500</a></li>
+                        <li><a href="#">Latha - 2500</a></li>
+                        <li><a href="#">Kyaukdadar - 2800</a></li>
+                        <li><a href="#">BotaHtaung - 3000</a></li>
+                        <li><a href="#">Bahan - 3500</a></li>
                     </ul>
                     <i class="arrow-main-menu fa fa-angle-right" aria-hidden="true"></i>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="product.html">Shop</a>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="product.html">Sale</a>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="cart.html">Features</a>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="blog.html">Blog</a>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="about.html">About</a>
-                </li>
-
-                <li class="item-menu-mobile">
-                    <a href="contact.html">Contact</a>
                 </li>
             </ul>
         </nav>
